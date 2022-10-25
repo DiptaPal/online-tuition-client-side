c266fbb591bcf98e4970e344affcaf76caf3ac7e
DiptaPal <diptapal1998@gmail.com>
2022-10-25 17:27:31 +0600

    footer section done

--- src/Pages/ErrorPage/ErrorPage.js
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import errorLogo  from "../../assets/error.json"
+import Lottie from 'lottie-react'
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h2>This is Error Page</h2>
+        <div className="w-screen h-screen flex justify-center items-center">
+            <section className="flex items-center h-full sm:p-16 text-gray-800">
+                <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8 space-y-8 text-center sm:max-w-md">
+                    <Lottie animationData={errorLogo} loop={true} />
+                    <p className="text-3xl">Looks like our services are currently offline</p>
+                    <Link to="/" className="text-2xl px-8 py-3 font-semibold rounded bg-navActive shadow-md hover:text-navActive hover:bg-white text-gray-50">Back to homepage</Link>
+                </div>
+            </section>
         </div>
     );
 };
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,126 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { BsFacebook, BsTwitter, BsGithub, BsLinkedin } from "react-icons/bs";
+import { FaAmazon } from "react-icons/fa";
+
 
 const Footer = () => {
     return (
         <div>
-            <h2>This is Footer</h2>
+            <footer className="bg-white pt-4 pb-8 xl:pt-8">
+                <div className="sm:max-w-xl md:max-w-full lg:max-w-screen-2xl mx-auto px-4 sm:px-6 md:px-8 text-gray-400 dark:text-gray-300">
+                    <ul className="text-lg font-light pb-8 flex flex-wrap justify-center">
+                        <li className="w-1/2 md:w-1/3 lg:w-1/3">
+                            <div className="text-center">
+                                <h2 className="text-navActive font-bold text-md uppercase mb-4">
+                                    About Us
+                                </h2>
+                                <ul>
+                                    <li className="mb-4 font-medium text-black hover:text-navActive transition-colors duration-300">
+                                        <Link to="#">
+                                            Company
+                                        </Link>
+                                    </li>
+                                    <li className="mb-4 font-medium text-black hover:text-navActive transition-colors duration-300">
+                                        <Link to="#">
+                                            Teams
+                                        </Link>
+                                    </li>
+                                    <li className="mb-4 font-medium text-black hover:text-navActive transition-colors duration-300">
+                                        <Link to="#">
+                                            Profile
+                                        </Link>
+                                    </li>
+                                    <li className="mb-4 font-medium text-black hover:text-navActive transition-colors duration-300">
+                                        <Link to="#">
+                                            Career
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li className="w-1/2 md:w-1/3 lg:w-1/3">
+                            <div className="text-center">
+                                <h2 className="text-navActive font-semibold text-md uppercase mb-4">
+                                    Contacts
+                                </h2>
+                                <ul>
+                                    <li className="mb-4 font-medium text-black hover:text-navActive transition-colors duration-300">
+                                        <Link to="#">
+                                            Github
+                                        </Link>
+                                    </li>
+                                    <li className="mb-4 font-medium text-black hover:text-navActive transition-colors duration-300">
+                                        <Link to="#">
+                                            Facebook
+                                        </Link>
+                                    </li>
+                                    <li className="mb-4 font-medium text-black hover:text-navActive transition-colors duration-300">
+                                        <Link to="#">
+                                            Twitter
+                                        </Link>
+                                    </li>
+                                    <li className="mb-4 font-medium text-black hover:text-navActive transition-colors duration-300">
+                                        <Link to="#">
+                                            LinkedIn
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li className="w-1/2 md:w-1/3 lg:w-1/3">
+                            <div className="text-center">
+                                <h2 className="text-navActive font-semibold text-md uppercase mb-4">
+                                    Courses
+                                </h2>
+                                <ul>
+                                    <li className="mb-4 font-medium text-black hover:text-navActive transition-colors duration-300">
+                                        <Link to="#">
+                                            Design
+                                        </Link>
+                                    </li>
+                                    <li className="mb-4 font-medium text-black hover:text-navActive transition-colors duration-300">
+                                        <Link to="#">
+                                            Development
+                                        </Link>
+                                    </li>
+                                    <li className="mb-4 font-medium text-black hover:text-navActive transition-colors duration-300">
+                                        <Link to="#">
+                                            Marketing
+                                        </Link>
+                                    </li>
+                                    <li className="mb-4 font-medium text-black hover:text-navActive transition-colors duration-300">
+                                        <Link to="#">
+                                            Blog
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                    <div className="pt-8 flex border-t border-navActive max-w-xs mx-auto items-center justify-between">
+                        <Link to="#">
+                            <BsFacebook className='text-2xl text-navActive'></BsFacebook>
+                        </Link>
+                        <Link to="#">
+                            <BsTwitter className='text-2xl text-navActive'></BsTwitter>
+                        </Link>
+                        <Link to="#">
+                            <BsGithub className='text-2xl text-navActive'></BsGithub>
+                        </Link>
+                        <Link to="#">
+                            <BsLinkedin className='text-2xl text-navActive'></BsLinkedin>
+                        </Link>
+                        <Link to="#">
+                           <FaAmazon className='text-3xl text-navActive'></FaAmazon>
+                        </Link>
+                    </div>
+                    <div className="text-center text-black font-normal pt-10 sm:pt-12 flex items-center justify-center">
+                        <p>© 2022 Online Study. All rights reserved.</p>
+                    </div>
+                </div>
+            </footer>
+
         </div>
     );
 };
