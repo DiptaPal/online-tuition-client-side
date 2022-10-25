ee56cb224670965bcd065d1fbe45f57446b3bad4
DiptaPal <diptapal1998@gmail.com>
2022-10-25 16:23:59 +0600

    stats section added

--- src/Pages/Home/Main/Home.js
+++ src/Pages/Home/Main/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
+import Stats from '../Stats/Stats';
 const Home = () => {
     return (
         <div>
             <Hero></Hero>
+            <Stats></Stats>
         </div>
     );
 };
